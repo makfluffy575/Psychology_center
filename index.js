6a188a94d6f40b1144e642ab1defc8a0c3a15415
makfluffy575 <fluffy.mk575@gmail.com>
2024-12-22 22:03:20 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function menuHidden () {
-  const menuList = document.querySelector('.header__menu-list');
-  if (menuList.style.display == 'block')
-    menuList.style.display = 'none';
-  else
-    menuList.style.display = 'block'
+function menuHidden() {
+  const menuList = document.querySelector(".header__menu-list");
+  if (menuList.style.display == "block") menuList.style.display = "none";
+  else menuList.style.display = "block";
 }
 
-document.querySelector('.header__burger').onclick = menuHidden;
-document.querySelector('.header__menu-list').onclick = menuHidden;
+document.querySelector(".header__burger").onclick = menuHidden;
 
-
+document.querySelector(".header__menu-list").addEventListener("click", () => {
+  document.querySelector(".header__burger").click();
+});
